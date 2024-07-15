4cd385ae8747d16daa376817283776eea4b44155
Archana Singh <archana21@navgurukul.org>
2024-07-15 17:28:32 +0530

    add some space

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">Welcome to Recipe website</h1>
+    <h1 className="text-3xl font-bold underline"> Welcome to Recipe websites </h1>
   );
 };
