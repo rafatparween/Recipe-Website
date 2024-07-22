1fa64c0291cb9c178cc80560169bc2982657d593
rafat22 <rafat22@navgurukul.org>
2024-07-22 22:52:10 +0530

    resolve an error

--- app/context/cartContext.js
+++ app/context/cartContext.js
@@ -6,7 +6,6 @@ const CartContext = createContext();
 export const CartProvider = ({ children }) => {
     const [cart, setCart] = useState([]);
 
-    // Function to add an item to the cart
     const addToCart = (item) => {
         setCart((prevCart) => [...prevCart, item]);
     };
@@ -18,7 +17,6 @@ export const CartProvider = ({ children }) => {
     );
 };
 
-// Custom hook to use the CartContext
 export const useCart = () => {
     const context = useContext(CartContext);
     if (!context) {
